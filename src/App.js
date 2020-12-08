7242b39a59b8decc24919ca35fab61847a423b90
Whitney Smith <whitney.smith.lee@gmail.com>
2020-12-07 18:40:13 -0800

    component folder strucutre complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/Header/Header';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="container">
+        <Container />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <h2>
+                &copy; Whitney Smith. 2020.
+            </h2>
+        </div>
+    )
+}
+
+export default Footer;

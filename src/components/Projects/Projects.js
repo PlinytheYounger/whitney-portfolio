233d563cc67bef8651b986efca9f7fd7ce16e2fe
Whitney Smith <whitney.smith.lee@gmail.com>
2021-05-25 08:48:42 -0700

    updated escape links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,25 +3,28 @@ import '../../css/projects.css';
 import ProjectElement from './Project-Element';
 import image1 from '../../images/photo1.jpeg';
 import image2 from '../../images/photo2.jpeg';
-import image3 from '../../images/photo3.jpeg';
+import image3 from '../../images/escape_home.png';
 
 
 const Projects = () => {
     const projectArr = [
         {
+            name: 'Escape',
+            websiteUrl: 'https://escape-travel-app.herokuapp.com/',
+            gitHubUrl: 'https://github.com/PlinytheYounger/escape-app',
+            description: 'All in one, easy to use travel trip planner. All of your lodging, receipts, tickets and information in one spot. Invite others from your group to join so you can easily split expenses and collaborate!',
+            photoUrl: image3  
+        },
+        {
             name: 'Study Buddy',
+            websiteUrl: '',
             gitHubUrl: 'https://github.com/PlinytheYounger/study_buddy_frontend',
             description: 'One stop shop to track your upcoming interviews, create a study plan and connect with others to schedule interview practice!',
             photoUrl: image2 
         },
         {
-            name: 'Travel Ease',
-            gitHubUrl: 'https://github.com/PlinytheYounger/escape-client',
-            description: 'All in one, easy to use travel trip planner. All of your lodging, receipts, tickets and information in one spot. Invite others from your group to join so you can easily split expenses and collaborate!',
-            photoUrl: image3  
-        },
-        {
             name: 'Lobbyists & Bills',
+            websiteUrl: '',
             gitHubUrl: '',
             description: 'Have you ever wanted to see exactly what your constituent brings to the floor once they are elected? Data trends for bills.',
             photoUrl: image1 
